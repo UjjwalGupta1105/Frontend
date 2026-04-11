e193653c6f72f53ebd288f262f3bdd232a710d95
Ujjwal Gupta <ujjwalgu000@gmail.com>
2026-04-11 20:29:47 +0530

    #fix: useCreateJob code maintained

--- src/utils/useCreateJob.ts
+++ src/utils/useCreateJob.ts
@@ -1,10 +1,12 @@
 import { jobServiceApi } from "@/lib/axios.config";
 import { CreateJobFormSchema } from "@/schema/createJob.validator";
+import { ApiResponse } from "@/types/ApiResponse";
 import { ErrorResponse } from "@/types/ErrorResponse";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { toast } from "sonner";
 import z from "zod";
+import { Job as JobType } from "@/types/GetJobType";
 
 type CreateJobFormValues = z.infer<typeof CreateJobFormSchema>;
 
@@ -18,12 +20,7 @@ const useCreateJob = () => {
       createJobData: CreateJobFormValues;
       authJwtToken: string | null;
     }) => {
-      const response = await jobServiceApi.post("/jobs", createJobData, {
-        headers: {
-          Authorization: `${authJwtToken}`,
-        },
-      });
-      return response.data;
+      return await createJob( createJobData,authJwtToken)
     },
     onError: (error: AxiosError<ErrorResponse>) => {
       const message =
@@ -39,5 +36,19 @@ const useCreateJob = () => {
     },
   });
 };
+
+interface Job{
+  jobRecord:JobType;
+  jobSkills: { id: number; name: string }[];
+}
+
+const createJob=async(createJobData: CreateJobFormValues, authJwtToken: string | null):Promise<ApiResponse<Job>>=>{
+  const response = await jobServiceApi.post("/jobs", createJobData, {
+        headers: {
+          Authorization: `${authJwtToken}`,
+        },
+      });
+      return response.data;
+}
 
 export default useCreateJob;
