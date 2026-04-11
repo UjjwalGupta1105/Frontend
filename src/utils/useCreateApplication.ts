a861e9981c0cec9d8bfa0507f98a5fac7b5c9640
Ujjwal Gupta <ujjwalgu000@gmail.com>
2026-04-11 18:00:37 +0530

    #fix: createApplication code enhanced

--- src/utils/useCreateApplication.ts
+++ src/utils/useCreateApplication.ts
@@ -1,4 +1,5 @@
 import { jobServiceApi } from "@/lib/axios.config"
+import { ApiResponse } from "@/types/ApiResponse"
 import { ErrorResponse } from "@/types/ErrorResponse"
 import { useMutation } from "@tanstack/react-query"
 import { AxiosError } from "axios"
@@ -10,21 +11,7 @@ const  useCreateApplication= ()=>{
             jobId: number | null,
             jwtToken: string | null
         })=>{
-            try{
-                if(!jwtToken){
-                    toast.error('Log in to continue')
-                    throw new Error("No jwt token provided");
-                }
-                const response= await jobServiceApi.post('/applications', {jobId}, {
-                    headers: {
-                        Authorization: jwtToken
-                    }
-                })
-
-                return response.data ;
-            }catch(error){
-                throw error ;
-            }
+            return await createApplication(jwtToken, jobId);
         },
         onError: (error: AxiosError<ErrorResponse>)=>{
             const message =
@@ -38,6 +25,31 @@ const  useCreateApplication= ()=>{
             toast.success(data.message || "Succesfully applied");
         }
     })
+}
+
+interface Application{
+   applied_at: string;
+   candidate_id: number;
+   deleted_at:string | null;
+   id: number;
+   job_id:number;
+}
+
+const createApplication=async(jwtToken: string | null ,jobId: number | null ):Promise<ApiResponse<Application>>=>{
+     try{
+            if(!jwtToken){
+                toast.error('Log in to continue')
+                throw new Error("No jwt token provided");
+            }
+            const response= await jobServiceApi.post('/applications', {jobId}, {
+                headers: {
+                    Authorization: jwtToken
+                }
+            })
+            return response.data ;
+        }catch(error){
+            throw error ;
+        }
 }
 
 export default useCreateApplication ;
